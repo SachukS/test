b37ee4b530a6fdeebdeb24c7a1bb1e8e0ad6bfc5
Oleksandr Sachuk <sachuk29sanya@gmail.com>
2022-09-20 11:50:53 +0300

    add validation for dto

--- src/main/java/com/sachuk/TestTask/dto/ProductDto.java
+++ src/main/java/com/sachuk/TestTask/dto/ProductDto.java
@@ -3,13 +3,18 @@ package com.sachuk.TestTask.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Data
 public class ProductDto {
     Long id;
+    @Size(min=5, message = "Name is too short, minimum 5 symbols")
     String name;
+    @Pattern(regexp = "^\\d+(.\\d{1,2})?$")
     String price;
+    @Size(min=10, message = "Description is too short, minimum 10 symbols")
     String description;
 
     @Override
--- src/main/java/com/sachuk/TestTask/dto/ShopDto.java
+++ src/main/java/com/sachuk/TestTask/dto/ShopDto.java
@@ -2,13 +2,16 @@ package com.sachuk.TestTask.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Size;
 import java.util.Objects;
 import java.util.Set;
 
 @Data
 public class ShopDto {
     Long id;
+    @Size(min=8, message = "Name is too short, minimum 8 symbols")
     String name;
+    @Size(min=20, message = "Address is too short, minimum 20 symbols")
     String address;
     Set<ProductDto> products;
 
